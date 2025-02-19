edc09b62631f6b820d27bf513040cfba18f790b9
Vardan Bansal <vardan.bansal@harness.io>
2025-02-18 17:09:22 -0800

    Adding multiple styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,23 @@ import { SharedStyle } from "./components/shared-style";
 // import { Button } from "@harnessio/ui/components";
 
 function App() {
+  const style = `
+      p { font-size: 16px; color: red; }
+      span { color: yellow; }
+    `;
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-6">
       {/* <Button variant="default" onClick={() => alert("clicked")}>
         Click me
       </Button> */}
       <ShadowWrapper>
-        <SharedStyle
-          styleId="text-color-red"
-          styleAsString={`p { color: red; }`}
-        />
+        <SharedStyle styleId="custom-style" styleAsString={style} />
         <p>Hello World!</p>
       </ShadowWrapper>
       <ShadowWrapper>
-        <SharedStyle styleId="text-color-red" />
+        <SharedStyle styleId="custom-style" />
         <p>42 is the answer to everything...</p>
+        <span>Do you know why?</span>
       </ShadowWrapper>
     </div>
   );
