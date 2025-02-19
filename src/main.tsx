25695e06b45e4c666947b4af04a5561b4aa7ceac
Vardan Bansal <vardan.bansal@harness.io>
2025-02-18 20:52:46 -0800

    Cleanup inline styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import SharedStyle from "./components/shared-style.tsx";
+/** import css as raw string: https://stackoverflow.com/questions/70982306/get-raw-string-value-by-import-with-vite */
 import styleCss from "@harnessio/ui/styles.css?inline";
 
-// const style1 = `
-// button { background-color: blue; color: white; }
-// p { font-size: 16px; color: red; }
-// `;
+const inlineStyle = `
+span { color: red; }
+p { font-size: 16px; color: blue; }
+`;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <SharedStyle styleId="custom-style-1" styleAsString={styleCss} />
+    <SharedStyle styleId="custom-style-2" styleAsString={inlineStyle} />
     <App />
-    <SharedStyle styleId="custom-style" styleAsString={styleCss} />
   </StrictMode>
 );
