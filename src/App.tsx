6cc984173444d827add424bc5171aa4c66fd3f94
Vardan Bansal <vardan.bansal@harness.io>
2025-02-18 19:49:23 -0800

    separating styles into two

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,25 @@ import SharedStyle from "./components/shared-style";
 import { Button } from "@harnessio/ui/components";
 
 function App() {
-  const style = `
+  const style1 = `
       button { background-color: blue; color: white; }
       p { font-size: 16px; color: red; }
-      span { color: yellow; }
-      h1 { color: green; }
     `;
+
+  const style2 = `
+    span { color: yellow; }
+    h1 { color: green; }
+  `;
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-6">
       <ShadowWrapper>
-        <SharedStyle styleId="custom-style" styleAsString={style} />
+        <SharedStyle styleId="custom-style-1" styleAsString={style1} />
         <Button variant="secondary" onClick={() => alert("clicked")}>
           Click me
         </Button>
       </ShadowWrapper>
       <ShadowWrapper>
+        <SharedStyle styleId="custom-style-2" styleAsString={style2} />
         <h1>First component</h1>
       </ShadowWrapper>
       <ShadowWrapper>
