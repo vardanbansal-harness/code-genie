9762fb484418c5108ae1f7c1dbc7145b0298b04d
Vardan Bansal <vardan.bansal@harness.io>
2025-02-18 20:35:17 -0800

    Integrate harnessio/ui css

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,21 @@
 import "./App.css";
 import ShadowWrapper from "./components/shadow-wrapper";
-import SharedStyle from "./components/shared-style";
 import { Button } from "@harnessio/ui/components";
 
 function App() {
-  const style1 = `
-      button { background-color: blue; color: white; }
-      p { font-size: 16px; color: red; }
-    `;
-
-  const style2 = `
-    span { color: yellow; }
-    h1 { color: green; }
-  `;
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-6">
       <ShadowWrapper>
-        <SharedStyle styleId="custom-style-1" styleAsString={style1} />
+        <Button variant="destructive" onClick={() => alert("clicked")}>
+          Click me
+        </Button>
+      </ShadowWrapper>
+      <ShadowWrapper>
         <Button variant="secondary" onClick={() => alert("clicked")}>
           Click me
         </Button>
       </ShadowWrapper>
       <ShadowWrapper>
-        <SharedStyle styleId="custom-style-2" styleAsString={style2} />
         <h1>First component</h1>
       </ShadowWrapper>
       <ShadowWrapper>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import SharedStyle from "./components/shared-style.tsx";
+import styleCss from "@harnessio/ui/styles.css?inline";
 
-createRoot(document.getElementById('root')!).render(
+// const style1 = `
+// button { background-color: blue; color: white; }
+// p { font-size: 16px; color: red; }
+// `;
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    <SharedStyle styleId="custom-style" styleAsString={styleCss} />
+  </StrictMode>
+);
